e02985e22da7670e33f30be89be5b7d7afa44d08
Ashutosh <being.ashutosh16.20@gmail.com>
2026-07-01 16:48:08 +0530

    middleware to proxy switch

---
+++ proxy.ts
@@ -0,0 +1,12 @@
+// proxy.ts
+
+import createMiddleware from "next-intl/middleware";
+import { routing } from "./i18n/routing";
+
+const proxy = createMiddleware(routing);
+
+export default proxy;
+
+export const config = {
+  matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
+};
